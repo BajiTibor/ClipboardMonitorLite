864acd63abb4fb75bfa70e95fa9ae3ee6442c5f9
EM Tibor <baji.tibor.gm@gmail.com>
2019-08-08 16:17:44 +0200

    Update SaveHistory.cs

--- ClipboardMonitorLite/NEWLY CREATED/File/SaveHistory.cs
+++ ClipboardMonitorLite/NEWLY CREATED/File/SaveHistory.cs
@@ -1,13 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace ClipboardMonitorLite
 {
-    class SaveHistory
+    public class SaveHistory
     {
+        ClipboardManager ClipManager;
+        public SaveHistory(ClipboardManager clipManager)
+        {
+            ClipManager = clipManager;
+            ClipManager.PropertyChanged += ClipManager_PropertyChanged;
+        }
 
+        private async void ClipManager_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals("CurrentlyCopiedItem"))
+            {
+                try
+                {
+                    using (StreamWriter sw = File.AppendText(FilePath))
+                    {
+                        await sw.WriteLineAsync(ClipManager.CurrentlyCopiedItem);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public string FilePath { get; set; }
+
+        
     }
 }

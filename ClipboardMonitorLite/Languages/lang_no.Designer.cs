03077d3cde7b89185354d19ea9a64094bd304731
EM Tibor <baji.tibor.gm@gmail.com>
2019-09-04 18:24:47 +0200

    Updated languages

--- ClipboardMonitorLite/Languages/lang_no.Designer.cs
+++ ClipboardMonitorLite/Languages/lang_no.Designer.cs
@@ -151,7 +151,7 @@ namespace ClipboardMonitorLite.Languages {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to NEEDS TRANSLATION.
+        ///   Looks up a localized string similar to Begrens datatrafikk.
         /// </summary>
         internal static string Check_LimitTraffic {
             get {

9b1095e83034ecfb4a0b548d98eebd53b2c0279f
EM Tibor <baji.tibor.gm@gmail.com>
2019-09-23 18:11:46 +0200

    Added api message and groupOptions

---
+++ CloudConnectionLib/Messages/ApiMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudConnectionLib.Messages
+{
+    public class ApiMessage
+    {
+        public Guid GroupId { get; set; }
+        public Guid ApplicationId { get; set; }
+        public string Password { get; set; }
+        public string Content { get; set; }
+    }
+}
